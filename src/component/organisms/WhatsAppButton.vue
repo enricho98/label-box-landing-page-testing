4f9bdf423b75be35c3db0834ed2bb3255e66a314
Enricho Rasimin <enshen0898@gmail.com>
2023-12-19 03:08:33 +0700

    add whatsapp button

--- src/component/organisms/WhatsAppButton.vue
+++ src/component/organisms/WhatsAppButton.vue
@@ -33,14 +33,26 @@
   </script>
   
   <style scoped>
-  .whatsapp-button {
-	position: fixed;
-	bottom: 20px;
-	right: 20px;
-	padding: 10px 20px;
-	font-size: 50px;
-	border: none;
-	color: #25d366;
-  }
+.whatsapp-button {
+  position: fixed;
+  bottom: 20px;
+  right: 20px;
+  width: 50px;
+  height: 50px;
+  border-radius: 50%;
+  border: none;
+  background-color: white;
+  color: #25d366;
+  font-size: 24px;
+  cursor: pointer;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+  transition: background-color 0.3s ease-in-out;
+}
+.whatsapp-button:hover {
+  background-color: #204280;
+}
   </style>
   
